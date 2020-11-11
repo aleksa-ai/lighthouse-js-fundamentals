963064bc03278090916db2bc58a0195823e0ed8f
Aleksa Gavrilovic <lks_g@live.ca>
2020-11-10 22:17:56 -0500

    playing with sayHello function

--- hello.js
+++ hello.js
@@ -14,8 +14,14 @@ sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");*/
 
-/*const returnSayHello  = function (name) {
+/*const sayHelloToConsole = function(name){
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');*/
+
+const returnSayHello = function (name){
   return "Hello, " + name;
 }
+
 const greeting = returnSayHello('John');
-console.log(greeting);*/
+console.log(greeting);
