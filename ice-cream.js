aa70c31531274848feea8946e5fac1bb2f3cc0f2
Aleksa Gavrilovic <lks_g@live.ca>
2020-11-16 16:32:58 -0500

    playing with arrays

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+//console.log(iceCreamFlavours);
+
+//Add Flavour
+iceCreamFlavours.push("root beer");
+//console.log(iceCreamFlavours);
+
+//First Flavour
+//console.log(iceCreamFlavours[0]);
+
+//Last Flavour
+//console.log(iceCreamFlavours[iceCreamFlavours.length-1]);
+
+//Total # Flavours
+console.log(iceCreamFlavours.length);
--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,15 @@
-var packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
-for (var i = 0; i < packingList.length; i++) {
+
+//Iterating Over an Array With for
+/*for(let i = 0; i < packingList.length; i++){
   console.log(packingList[i]);
+}*/
+
+//Iterating Over an Array With while
+let i=0
+while (i < packingList.length){
+  console.log(packingList[i])
+  i++;
 }
