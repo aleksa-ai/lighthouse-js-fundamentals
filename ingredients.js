9652b0365dddcfa0caf75c3502cf18cac0cffdeb
Aleksa Gavrilovic <lks_g@live.ca>
2020-11-16 16:53:55 -0500

    Iterating Over Arrays: Banana Bread

--- ingredients.js
+++ ingredients.js
@@ -1,18 +1,20 @@
-var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
 // Write a while loop that prints out the contents of ingredients:
-var  i = 0;
+
+/*let i = 0
 while (i < ingredients.length){
   console.log(ingredients[i]);
   i++;
-}
+}*/
+
 // Write a for loop that prints out the contents of ingredients:
 
-for(var j = 0; j < ingredients.length; j++){
-  console.log(ingredients[j]);
-}
+/*for(i = 0; i < ingredients.length; i++){
+  console.log(ingredients[i]);
+}*/
 
 // Write any loop (while or for) that prints out the contents of ingredients backwards:
-for(var k = ingredients.length; k > 0; k--){
-  console.log(ingredients[k-1]);
+for(let i = ingredients.length; i > 0; i--){
+  console.log(ingredients[i-1]);
 }
