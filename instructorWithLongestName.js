933c3c0e66896109a61acd0d12d1530f2504ab55
Aleksa Gavrilovic <lks_g@live.ca>
2020-11-18 18:44:25 -0500

    Created instructorWithLongestName function

---
+++ instructorWithLongestName.js
@@ -0,0 +1,30 @@
+const instructorWithLongestName = function(instructors) {
+  // Put your solution here
+  let longestNameLenght = 0;
+  let longestNameLenghtIndex = 0;
+  for (let i = 0; i < instructors.length; i++){
+    if((instructors[i].name).length > longestNameLenght){
+      longestNameLenght = (instructors[i].name).length;
+      longestNameLenghtIndex = i;
+    }
+  }
+  return instructors[longestNameLenghtIndex];
+};
+
+console.log(instructorWithLongestName([
+  {name: "Samuel", course: "iOS"},
+  {name: "Jeremiah", course: "Web"},
+  {name: "Ophilia", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(instructorWithLongestName([
+  {name: "Matthew", course: "Web"},
+  {name: "David", course: "iOS"},
+  {name: "Domascus", course: "Web"}
+]));
+
+/*
+Expected output:
+{name: "Jeremiah", course: "Web"}
+{name: "Domascus", course: "Web"}
+*/
