f6116294f4751ec28b222c5b98d6c3ae8f3c6716
Aleksa Gavrilovic <lks_g@live.ca>
2020-11-18 17:36:15 -0500

    Created consitionalSum function

---
+++ conditionalSum.js
@@ -0,0 +1,28 @@
+const conditionalSum = function(values, condition) {
+  // Your code here
+  let sum = 0
+  for(let i = 0; i < values.length; i++){
+    if (condition === "even"){
+      if(values[i] % 2 === 0){
+        sum += values[i];
+      }
+    }else /*if(condition === "odd")*/{
+      if(values[i] % 2 === 1){
+        sum += values[i];
+      }
+    }
+  }
+  return sum
+};
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
+
+/*Expected Output:
+6
+9
+144
+0
+*/
